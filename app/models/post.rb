0b6190a963b365116c523569bd91379195b6133a
Sylvain UTARD <sylvain.utard@gmail.com>
2013-09-28 18:43:43 +0200

    Seems that the ruby client forgot somethings

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,6 @@ class Post < ActiveRecord::Base
   include AlgoliaSearch
 
   algoliasearch synchronous: false do
-    attributesToIndex = ["title", "url", "author"]
-    customRanking = ["desc(points)", "asc(best_rank)"]
   end
 
 end
