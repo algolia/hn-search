f97d634ba863871f81d674f44ccc016668b62859
Sylvain UTARD <sylvain.utard@gmail.com>
2013-09-28 18:21:14 +0200

    No need to be synchronous here

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   include AlgoliaSearch
 
-  algoliasearch do
+  algoliasearch synchronous: false do
     attributesToIndex = ["title", "url", "author"]
     customRanking = ["desc(points)", "desc(best_rank)"]
   end
