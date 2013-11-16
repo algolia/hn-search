19de90acf38ba5d8bdd7995170e26d10c86d615d
Sylvain UTARD <sylvain.utard@gmail.com>
2013-11-16 15:08:52 +0100

    Revert "I need to path this helper to not fail if the env variables are missing"
    This reverts commit 6b7fe0dcc7be57a0bd9469f518ea82c4201b6baa.

--- app/models/my_model.rb
+++ app/models/my_model.rb
@@ -1,10 +1,6 @@
 class MyModel < ActiveRecord::Base
 
-  if ENV['ALGOLIASEARCH_APPLICATION_ID']
-    include AlgoliaSearch
-
-    algoliasearch auto_index: false, auto_remove: false do
-    end
+  algoliasearch auto_index: false, auto_remove: false do
   end
 
 end
