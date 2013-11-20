181c4f1ed98fb0d083993faf7a2c8973194fcfe6
Sylvain UTARD <sylvain.utard@gmail.com>
2013-11-20 20:58:28 +0100

    Second try

--- db/migrate/20131120195227_hn_id_is_an_integer.rb
+++ db/migrate/20131120195227_hn_id_is_an_integer.rb
@@ -1,5 +1,5 @@
 class HnIdIsAnInteger < ActiveRecord::Migration
   def change
-    change_column :posts, :hn_id, :integer
+    change_column :posts, :hn_id, 'integer USING CAST(hn_id AS integer)'
   end
 end
