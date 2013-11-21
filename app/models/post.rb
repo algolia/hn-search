92a1a06fd57fb9e14b79c7235a0892feae236a0f
Sylvain UTARD <sylvain.utard@gmail.com>
2013-11-21 08:09:36 +0100

    Disable proximity and positions, set a list of optional words

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,21 @@
 class Post < ActiveRecord::Base
-
   include AlgoliaSearch
 
   algoliasearch per_environment: true do
-    attributesToIndex ["title", "source", "url", "author"]
-    customRanking ["desc(points)", "asc(best_rank)"]
+    attributesToIndex ['unordered(title)', 'source', 'url', 'author']
+    customRanking ['desc(points)']
+    ranking ['typo', 'attribute', 'exact', 'custom']
+    queryType 'prefixAll'
+    separatorsToIndex '+#$'
+    optionalWords %w{a about above after again against all am an and any are aren as at be because
+      been before being below between both but by can cannot could couldn d did didn do does doesn
+      doing don down during each few for from further had hadn has hasn have haven having he her
+      here herself him himself his how how i if in into is isn it its itself let ll m me more
+      most mustn my myself no nor not of off on once only or other ought our ours ourselves out
+      over own re re re s same shan she should shouldn so some such t than that that the their
+      theirs them themselves then there there these they this those through to too under until 
+      up ve very was wasn we were weren what when where which while who whom why with won would
+      wouldn you your yours yourself yourselves}
   end
 
   def self.from_json!(e, force = false)
