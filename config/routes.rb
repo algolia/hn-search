76e2b7b97851b4099ea72b1a86727dccd5132e7d
Sylvain UTARD <sylvain.utard@gmail.com>
2013-12-03 16:26:05 +0100

    Static pages draft

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Hnsearch::Application.routes.draw do
 
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
-      get 'items/:id', controller: 'items', action: 'show'
-      get 'users/:username', controller: 'users', action: 'show'
-      get 'search', controller: 'search', action: 'perform'
+      get 'items/:id', as: :items, controller: 'items', action: 'show'
+      get 'users/:username', as: :users, controller: 'users', action: 'show'
+      get 'search', as: :search, controller: 'search', action: 'perform'
     end
   end
 
