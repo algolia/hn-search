b61c0326a80719995cef8b801235807921fcafcc
Sylvain UTARD <sylvain.utard@gmail.com>
2013-12-04 20:31:38 +0100

    Cron every minute, crawl every 10 seconds

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,6 @@ set :output, "log/cron.log"
 
 job_type :runner, "source ${HOME}/.rvm/scripts/rvm && cd :path && bundle exec rails runner -e :environment ':task' :output"
 
-every 10.seconds, roles: [:cron] do
+every 1.minute, roles: [:cron] do
   runner "HackerNewsRealtimeCrawler.cron"
 end
