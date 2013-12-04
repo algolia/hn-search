df83cb7642ee1f84f5d6e42138483a7c37fab4f2
Sylvain UTARD <sylvain.utard@gmail.com>
2013-12-04 22:43:33 +0100

    Force unscoped

--- app/models/item.rb
+++ app/models/item.rb
@@ -42,7 +42,7 @@ class Item < ActiveRecord::Base
   end
 
   def num_comments
-    item_type == 'story' ? story_comments.size : nil
+    Item.unscoped { item_type == 'story' ? story_comments.count : nil }
   end
 
   def _tags
