779eaf24aa30db3b513d438161cdda86201b418e
Sylvain UTARD <sylvain.utard@gmail.com>
2013-12-04 17:30:14 +0100

    HackerNewsRealtimeCrawler

--- app/workers/hacker_news_realtime_crawler.rb
+++ app/workers/hacker_news_realtime_crawler.rb
@@ -1,8 +1,24 @@
 class HackerNewsRealtimeCrawler
 
+  MIN_REFRESH_DELAY = 10
+  REFRESH_CYCLE = 48
+  LIMIT = 1000
+
   def self.cron
     last_id = Item.order('id DESC').first.try(:id) || 1
-    
+
+    now = DateTime.now
+    sec = now.second / MIN_REFRESH_DELAY * MIN_REFRESH_DELAY
+    last_id -= case sec
+    when 0, 20, 40
+      # refresh news from last_id
+      0
+    else
+      # refresh news from last_id-n*LIMIT -> (last_id-n*LIMIT)+LIMIT
+      n = (now.minute * 6 + sec / 10) / 2 % REFRESH_CYCLE
+      n * LIMIT
+    end
+
     Item.refresh_since!(last_id)
   end
 
