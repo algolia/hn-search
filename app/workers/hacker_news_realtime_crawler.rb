5726c105bde6d6fa2b39031526a235d545b84054
Sylvain UTARD <sylvain.utard@gmail.com>
2013-12-05 20:20:48 +0100

    For now, start with a 2-min refresh rate

--- app/workers/hacker_news_realtime_crawler.rb
+++ app/workers/hacker_news_realtime_crawler.rb
@@ -1,35 +1,56 @@
 class HackerNewsRealtimeCrawler
 
-  MIN_REFRESH_DELAY = 10
   REFRESH_CYCLE = 48
   LIMIT = 1000
   CURSOR_FILE = '/tmp/hnsearch_cursor'
 
   def self.cron
-    6.times do |t|
-      last_id = Item.order('id DESC').first.try(:id) || 1
+    last_id = Item.order('id DESC').first.try(:id) || 1
 
-      last_id -= case t
-      when 0, 2, 4
-        # refresh news from last_id
-        0
-      else
-        # refresh news from last_id-n*LIMIT -> (last_id-n*LIMIT)+LIMIT
-        n = File.read(CURSOR_FILE).to_i rescue 0
-        n = 0 if n >= REFRESH_CYCLE
-        File.open(CURSOR_FILE, 'w') { |f| f << (n + 1).to_s }
-        n * LIMIT
-      end
+    last_id -= if DateTime.now.minute % 4 == 0
+      # refresh news from last_id-(LIMIT / 2)
+      LIMIT / 2
+    else
+      # refresh news from last_id-n*LIMIT -> (last_id-n*LIMIT)+LIMIT
+      n = File.read(CURSOR_FILE).to_i rescue 0
+      n = 0 if n >= REFRESH_CYCLE
+      File.open(CURSOR_FILE, 'w') { |f| f << (n + 1).to_s }
+      n * LIMIT
+    end
 
-      begin
-        items = Item.refresh_since!(last_id)
-        Item.where(id: items.map { |i| i.id }).reindex!
-      rescue Exception => e
-        puts "Failed to refresh #{last_id}: #{e}"
-      end
-
-      sleep 10
+    begin
+      items = Item.refresh_since!(last_id)
+      Item.where(id: items.map { |i| i.id }).reindex!
+    rescue Exception => e
+      puts "Failed to refresh #{last_id}: #{e}"
     end
   end
+
+  # def self.cron
+  #   6.times do |t|
+  #     last_id = Item.order('id DESC').first.try(:id) || 1
+
+  #     last_id -= case t
+  #     when 0, 2, 4
+  #       # refresh news from last_id
+  #       0
+  #     else
+  #       # refresh news from last_id-n*LIMIT -> (last_id-n*LIMIT)+LIMIT
+  #       n = File.read(CURSOR_FILE).to_i rescue 0
+  #       n = 0 if n >= REFRESH_CYCLE
+  #       File.open(CURSOR_FILE, 'w') { |f| f << (n + 1).to_s }
+  #       n * LIMIT
+  #     end
+
+  #     begin
+  #       items = Item.refresh_since!(last_id)
+  #       Item.where(id: items.map { |i| i.id }).reindex!
+  #     rescue Exception => e
+  #       puts "Failed to refresh #{last_id}: #{e}"
+  #     end
+
+  #     sleep 10
+  #   end
+  # end
 
 end
--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,6 @@ set :output, "log/cron.log"
 
 job_type :runner, "source ${HOME}/.rvm/scripts/rvm && cd :path && bundle exec rails runner -e :environment ':task' :output"
 
-every 1.minute, roles: [:cron] do
+every 2.minute, roles: [:cron] do
   runner "HackerNewsRealtimeCrawler.cron"
 end
