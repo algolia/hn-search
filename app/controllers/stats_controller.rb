8f7131444786759b9ff9e200cde342a1000a7b61
Sylvain UTARD <sylvain.utard@gmail.com>
2013-12-06 16:40:55 +0100

    Cache SQL query too.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,6 +1,4 @@
 class StatsController < ApplicationController
   def index
-    @stories = Item.stories_per_hour_since(1.month.ago)
-    @comments = Item.comments_per_hour_since(1.month.ago)
   end
 end
