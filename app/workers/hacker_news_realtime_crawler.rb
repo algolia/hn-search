eaebdddc86389b3f16df5833403317f240900296
Sylvain UTARD <sylvain.utard@gmail.com>
2014-01-13 11:23:01 -0800

    dead code

--- app/workers/hacker_news_realtime_crawler.rb
+++ app/workers/hacker_news_realtime_crawler.rb
@@ -31,31 +31,5 @@ class HackerNewsRealtimeCrawler
     Item.where(item_type_cd: Item.story).where('id > ?', first.id).reindex!
   end
 
-  # def self.cron
-  #   6.times do |t|
-  #     last_id = Item.order('id DESC').first.try(:id) || 1
-
-  #     last_id -= case t
-  #     when 0, 2, 4
-  #       # refresh news from last_id
-  #       0
-  #     else
-  #       # refresh news from last_id-n*LIMIT -> (last_id-n*LIMIT)+LIMIT
-  #       n = File.read(CURSOR_FILE).to_i rescue 0
-  #       n = 0 if n >= REFRESH_CYCLE
-  #       File.open(CURSOR_FILE, 'w') { |f| f << (n + 1).to_s }
-  #       n * LIMIT
-  #     end
-
-  #     begin
-  #       items = Item.refresh_since!(last_id)
-  #       Item.where(id: items.map { |i| i.id }).reindex!
-  #     rescue Exception => e
-  #       puts "Failed to refresh #{last_id}: #{e}"
-  #     end
-
-  #     sleep 10
-  #   end
-  # end
 
 end
