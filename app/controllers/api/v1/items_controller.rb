ccf1af8ad14cda901b8868f0180ff46fc14d09f5
Sylvain UTARD <sylvain.utard@gmail.com>
2014-01-24 11:54:13 -0800

    Skip deleted items

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -5,7 +5,7 @@ module Api
       def show
         id = params.delete :id
         json_request(:get, "/1/indexes/#{Item.index_name}/#{id}", params) # rate-limits check
-        render json: Item.find(id), root: false
+        render json: Item.where(deleted: false).find(id), root: false
       rescue Algolia::AlgoliaProtocolError => e
         render json: JSON.parse(client.body), status: e.code
       end
