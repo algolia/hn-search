7762440d820ae4380a8671bd6efa855179ad7847
Sylvain UTARD <sylvain.utard@gmail.com>
2014-02-07 13:24:03 -0800

    Do not serialize deleted/dead items

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -7,6 +7,10 @@ class ItemSerializer < ActiveModel::Serializer
     object.item_type
   end
 
+  def children
+    object.children.where(deleted: false).where(dead: false)
+  end
+
   def filter(keys)
     case object.item_type
     when 'comment'
