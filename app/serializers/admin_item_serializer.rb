486bd115765d5f1215d641c463d324b6042a0d3d
Sylvain UTARD <sylvain.utard@gmail.com>
2014-02-09 10:17:22 -0800

    API: polls now embed pollopt items. Fix #2

--- app/serializers/admin_item_serializer.rb
+++ app/serializers/admin_item_serializer.rb
@@ -1,10 +1,24 @@
 class AdminItemSerializer < ActiveModel::Serializer
-  attributes :id, :created_at, :deleted, :dead, :author, :title, :url, :text, :points, :parent_id
+  attributes :id, :created_at, :type, :deleted, :dead, :author, :title, :url, :text, :points, :parent_id
 
   has_many :children, serializer: AdminItemSerializer
+  has_many :options, serializer: AdminItemSerializer
 
   def type
     object.item_type
+  end
+
+  def children
+    object.children.where.not(item_type_cd: Item.pollopt)
+  end
+
+  def options
+    object.children.where(item_type_cd: Item.pollopt)
+  end
+
+  def filter(keys)
+    keys -= [:options] if object.item_type != 'poll'
+    keys
   end
 
 end
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,21 +1,25 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :created_at, :author, :title, :url, :text, :points
+  attributes :id, :created_at, :type, :author, :title, :url, :text, :points
 
   has_many :children
+  has_many :options
 
   def type
     object.item_type
   end
 
   def children
-    object.children.where(deleted: false).where(dead: false)
+    object.children.where(deleted: false).where(dead: false).where.not(item_type_cd: Item.pollopt)
+  end
+
+  def options
+    object.children.where(deleted: false).where(dead: false).where(item_type_cd: Item.pollopt)
   end
 
   def filter(keys)
-    case object.item_type
-    when 'comment'
-      keys -= [:url, :title]
-    end
+    keys -= [:url, :title] if object.item_type == 'comment' || object.item_type == 'pollopt'
+    keys -= [:options] if object.item_type != 'poll'
+    keys -= [:children, :author, :created_at] if object.item_type == 'pollopt'
     keys
   end
 
