56b8f78d236c23d8e1256e8176d254c063c4718e
Sylvain UTARD <sylvain.utard@gmail.com>
2014-03-07 23:09:59 -0800

    /items API now includes parent_id and story_id attributes

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,5 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :created_at, :created_at_i, :type, :author, :title, :url, :text, :points
+  attributes :id, :created_at, :created_at_i, :type, :author, :title, :url, :text, :points, :parent_id, :story_id
 
   has_many :children
   has_many :options
@@ -22,6 +22,7 @@ class ItemSerializer < ActiveModel::Serializer
 
   def filter(keys)
     return [:id, :children] if object.deleted || object.dead
+    keys -= [:parent_id, :story_id] if object.item_type == 'story' || object.item_type == 'poll'
     keys -= [:url, :title] if object.item_type == 'comment' || object.item_type == 'pollopt'
     keys -= [:options] if object.item_type != 'poll'
     keys -= [:children, :author, :created_at] if object.item_type == 'pollopt'
