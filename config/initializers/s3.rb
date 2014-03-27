7d7195ff6dc6fc2aaf8c5e13617581ea43213742
Sylvain UTARD <sylvain.utard@gmail.com>
2014-03-26 22:45:57 -0700

    Not fatal

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,1 +1,1 @@
-AWS::S3::Base.establish_connection!(access_key_id: ENV['AMAZON_ACCESS_KEY_ID'], secret_access_key: ENV['AMAZON_SECRET_ACCESS_KEY'])
+AWS::S3::Base.establish_connection!(access_key_id: ENV['AMAZON_ACCESS_KEY_ID'], secret_access_key: ENV['AMAZON_SECRET_ACCESS_KEY']) if ENV['AMAZON_ACCESS_KEY_ID']
