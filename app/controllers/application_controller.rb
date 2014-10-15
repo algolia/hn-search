e5ee754850d0855b41cbf9e04388b82e59b5d20a
Sylvain UTARD <sylvain.utard@gmail.com>
2014-10-15 14:48:14 +0200

    Allow algolia.com iframe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  after_action :allow_algolia_iframe
+
+  private
+  def allow_algolia_iframe
+    response.headers.delete('X-Frame-Options')
+  end
 end
