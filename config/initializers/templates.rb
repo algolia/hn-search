d27a5622024a561ba2d7c4bdb036ceb2ae2be2ec
Sylvain UTARD <sylvain.utard@gmail.com>
2015-01-02 14:25:54 +0100

    Useless

--- config/initializers/templates.rb
+++ config/initializers/templates.rb
@@ -1,6 +1,5 @@
 Rails.application.assets.context_class.class_eval do
   include ApplicationHelper
   include ActionView::Helpers
-  include AbstractController::Rendering
   include Rails.application.routes.url_helpers
 end
