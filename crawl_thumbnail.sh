1db7126b0592e1526542ae0dd4d47cc1365cfffe
Sylvain UTARD <sylvain.utard@gmail.com>
2015-01-08 17:36:38 +0100

    New thumbnails

--- crawl_thumbnail.sh
+++ crawl_thumbnail.sh
@@ -5,25 +5,29 @@ if [ $# -ne 2 ]; then
   exit 1
 fi
 
+BIN=wkhtmltoimage
+ROOT=`dirname "$0"`
 case `uname -a` in
   Darwin*)
-    BIN=wkhtmltoimage-mac
+    #BIN=$ROOT/wkhtmltoimage-mac
     TIMEOUT=gtimeout
     RUN=
     OPTIONS=
     ;;
   *)
-    BIN=wkhtmltoimage-amd64
+    #BIN=$ROOT/wkhtmltoimage-amd64
     TIMEOUT=timeout
     RUN="xvfb-run --auto-servernum --server-args=\"-screen 0, 1024x768x24\""
     OPTIONS="--use-xserver"
     ;;
 esac
-ROOT=`dirname "$0"`
 
 make_thumb() {
   convert "$1" -resize "$3^" -gravity North -crop "$3"+0+0 "$2"
+  if [ ! -z "$4" ]; then
+    convert "$2" "$4" -compose over -composite "$2"
+  fi
 }
 
-CMD="($TIMEOUT 60 $RUN \"$ROOT/$BIN\" --height 768 $OPTIONS --javascript-delay 30000 \"$1\" /tmp/$2-orig.png || $TIMEOUT 60 $RUN \"$ROOT/$BIN\" --height 768 $OPTIONS --disable-javascript \"$1\" /tmp/$2.png) && make_thumb /tmp/$2-orig.png /tmp/$2-240x180.png 240x180 && make_thumb /tmp/$2-orig.png /tmp/$2-600x315.png 600x315 && make_thumb /tmp/$2-orig.png /tmp/$2.png 100x100"
+CMD="($TIMEOUT 60 $RUN \"$BIN\" --height 768 $OPTIONS --javascript-delay 30000 \"$1\" /tmp/$2-orig.png || $TIMEOUT 60 $RUN \"$BIN\" --height 768 $OPTIONS --disable-javascript \"$1\" /tmp/$2-orig.png || test -f /tmp/$2-orig.png) && make_thumb /tmp/$2-orig.png /tmp/$2-240x180.png 240x180 \"$ROOT/app/assets/images/hn-mask-240x180.png\" && make_thumb /tmp/$2-orig.png /tmp/$2-600x315.png 600x315 \"$ROOT/app/assets/images/hn-mask-600x315.png\" && make_thumb /tmp/$2-orig.png /tmp/$2.png 100x100"
 eval $CMD
