e68a874f256c25d6dcea7a9a8e0fa3ea2d387710
Sylvain UTARD <sylvain.utard@gmail.com>
2016-04-25 10:51:00 +0200

    Use an environement variable for that

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Hnsearch::Application.config.secret_key_base = '7c595a8c9befb8205d9cefc9ccbe7827fa8d48e2b75ed497cfc61b7940fef908c255ff087498dc176e952211cf896dd3ff2f5b88a9da65da83cc7a2f57c1b45d'
+Hnsearch::Application.config.secret_key_base = ENV['SECRET_TOKEN']
