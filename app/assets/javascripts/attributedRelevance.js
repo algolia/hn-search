126c4dce92edef2c136efeb42acd358d1c93ec3f
Jonas <jonas.badalic@algolia.com>
2017-04-23 14:36:18 +0200

    update

--- app/assets/javascripts/attributedRelevance.js
+++ app/assets/javascripts/attributedRelevance.js
@@ -742,6 +742,7 @@ function sendReport() {
   // Open request
   var report = new XMLHttpRequest();
   report.open('GET', reportingQuery, false);
+  report.timeout = 2000;
   report.send();
 
   // Reset data
