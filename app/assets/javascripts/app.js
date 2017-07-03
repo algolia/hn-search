62f482d454fb77e68c105b9e472799213ec34649
Jonas <jonas.badalic@algolia.com>
2017-07-03 18:06:33 +0200

    update relevancy

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -175,6 +175,19 @@ $(document).ready(function() {
     }
   });
 
+  // var relevancyClass = new window.attributedRelevance({
+  //   hitsContainer: 'search-results',
+  //   inputSelector: '.page-header .search-wrapper input',
+  //   options: {
+  //     applicationID: 'UJ5WYC0L7X'
+  //   }
+  // })
+  var relevancy = new algoliaRelevance.algoliaRelevance({
+    indexName: "Item_production_ordered"
+  })
+
+  relevancy.initSearch('.page-header .search-wrapper input')
+
   //scroll top
   var scrollTopBtn = $('.scroll-to-top');
   scrollTopBtn.on('click', function(e){
