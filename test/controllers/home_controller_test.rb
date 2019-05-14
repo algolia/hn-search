400dd699f96755c7f869c2ba0c4117b1f7090bff
Jonas Badalic <jonas.badalic@algolia.com>
2019-05-14 13:39:45 +0200

    dep(quiet-assets): remove gem, option is now available out of the box

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
   end
-
 end
