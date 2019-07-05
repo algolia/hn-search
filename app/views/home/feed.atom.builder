1722d46b84302f615926a06528c8a6440015d8fa
Jonas Badalic <jonas.badalic@algolia.com>
2019-07-04 16:16:34 -1000

    fix(rss): fix fields as per atom feed spec

--- app/views/home/feed.atom.builder
+++ app/views/home/feed.atom.builder
@@ -1,15 +1,17 @@
 atom_feed do |feed|
   feed.title(@title)
   feed.updated(@updated_at)
+  feed.category(term: 'technology news')
 
   @stories.each do |story|
     feed.entry(story, url: "https://news.ycombinator.com/item?id=#{story.id}") do |entry|
-      entry.url(story.url)
-      entry.title(story.title)
+      entry.title(story.title, type: 'html')
+      entry.author do |author|
+        author.name(story.author)
+      end
+
+      entry.link(href: story.url)
       entry.content(story.text, type: 'html')
-      entry.comments(story.num_comments)
-      entry.points(story.points)
-      entry.author(story.author)
     end
   end
 end
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,15 +1,15 @@
-set :output, "log/cron.log"
+set :output, 'log/cron.log'
 set :job_template, "bash -c ':job'"
 
 job_type :runner, "source ${HOME}/.rvm/scripts/rvm && cd :path && bundle exec rails runner -e :environment ':task' :output"
 
 every 1.minute, roles: [:cron] do
-  runner "HackerNewsRealtimeCrawler.new.sanity_check(100)"
-  runner "HackerNewsRealtimeCrawler.refresh_home_page!"
+  runner 'HackerNewsRealtimeCrawler.new.sanity_check(100)'
+  runner 'HackerNewsRealtimeCrawler.refresh_home_page!'
   runner "User.where('updated_at >= ?', 5.minute.ago).reindex!"
   runner "Item.where('updated_at >= ?', 5.minute.ago).reindex!"
 end
 
 every 10.minute, roles: [:cron] do
-  runner "HackerNewsRealtimeCrawler.new.indexing_check"
+  runner 'HackerNewsRealtimeCrawler.new.indexing_check'
 end
