d0a7d4ceb5d371647936b5af4c213e128b83b0d6
Jonas Badalic <jonas.badalic@algolia.com>
2019-07-16 16:52:22 +0200

    fix

--- app/javascript/components/SearchResults/SearchResults.tsx
+++ app/javascript/components/SearchResults/SearchResults.tsx
@@ -7,9 +7,10 @@ import { SearchContext } from "../../providers/SearchProvider";
 
 const SearchResults: React.FunctionComponent = () => {
   const { results, loading } = React.useContext(SearchContext);
+  const hitsContainerRef = React.useRef();
 
   return (
-    <section className="SearchResults">
+    <section className="SearchResults" ref={hitsContainerRef}>
       {(!results.hits || !results.hits.length) && !loading && <NoResults />}
       {results.hits.map(hit => (
         <Story hit={hit} key={hit.objectID} />
