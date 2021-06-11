0326bc320e72a3c31476bf6bc5e31670e1c5dccd
Sylvain UTARD <sylvain.utard@gmail.com>
2021-06-11 11:39:49 +0200

    Make the H logo link to news.ycombinator.com

--- app/javascript/components/Header/Header.tsx
+++ app/javascript/components/Header/Header.tsx
@@ -7,14 +7,18 @@ const Header: React.FC = ({ children }) => {
   return (
     <header className="SearchHeader container">
       <div className="SearchHeader_search">
-        <Link className="SearchHeader_logo" to="/">
-          <img src={HNSearchLogo} />
-          <div className="SearchHeader_label">
-            Search
-            <br />
-            Hacker News
-          </div>
-        </Link>
+        <span className="SearchHeader_logo">
+          <a href="https://news.ycombinator.com">
+            <img src={HNSearchLogo} />
+          </a>
+          <Link to="/">
+            <div className="SearchHeader_label">
+              Search
+              <br />
+              Hacker News
+            </div>
+          </Link>
+        </span>
         {children}
       </div>
     </header>
